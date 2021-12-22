c27d01c74a76f728ef7bf8b1e6ddb2606e0c5fc6
Daniel Balchev <dbalchev@gmail.com>
2021-12-22 10:03:31 +0200

    Day 22 task 2

--- src/day_22.rs
+++ src/day_22.rs
@@ -1,16 +1,54 @@
-use std::{io::BufRead, ops::RangeInclusive};
+use core::num;
+use std::{
+    collections::HashMap,
+    io::BufRead,
+    ops::{Range, RangeInclusive},
+};
 
 use crate::common::MyResult;
+use itertools::Itertools;
 use ndarray::{s, Array3};
 use regex::Regex;
 use std::str::FromStr;
 
-#[derive(Debug)]
-struct Instruction {
+#[derive(Debug, Clone)]
+struct Instruction<R: Clone> {
     new_value: bool,
-    xs: RangeInclusive<i32>,
-    ys: RangeInclusive<i32>,
-    zs: RangeInclusive<i32>,
+    xs: R,
+    ys: R,
+    zs: R,
+}
+
+struct Remapper {
+    m: HashMap<i32, usize>,
+    numbers: Vec<i32>,
+}
+
+impl Remapper {
+    fn new(ranges: impl Iterator<Item = RangeInclusive<i32>>) -> Self {
+        let mut numbers = ranges
+            .flat_map(|a| [*a.start(), *a.end() + 1])
+            .collect_vec();
+        numbers.sort();
+        numbers.dedup();
+        Remapper {
+            m: numbers
+                .iter()
+                .cloned()
+                .enumerate()
+                .map(|(i, v)| (v, i))
+                .collect(),
+            numbers,
+        }
+    }
+
+    fn map(&self, r: RangeInclusive<i32>) -> Range<usize> {
+        self.m[&r.start()]..self.m[&(r.end() + 1)]
+    }
+
+    fn region_size(&self, r: usize) -> i32 {
+        self.numbers[r + 1] - self.numbers[r]
+    }
 }
 
 pub fn run_me(reader: impl BufRead) -> MyResult<()> {
@@ -61,6 +99,59 @@ pub fn run_me(reader: impl BufRead) -> MyResult<()> {
     }
 
     println!("Task 1: {}", reactor.mapv(|x| x as i32).sum());
+
+    let x_remapper = Remapper::new(instructions.iter().map(|i| i.xs.clone()));
+    let y_remapper = Remapper::new(instructions.iter().map(|i| i.ys.clone()));
+    let z_remapper = Remapper::new(instructions.iter().map(|i| i.zs.clone()));
+
+    let remapped_instructions = instructions
+        .iter()
+        .cloned()
+        .map(
+            |Instruction {
+                 new_value,
+                 xs,
+                 ys,
+                 zs,
+             }| Instruction {
+                new_value,
+                xs: x_remapper.map(xs),
+                ys: y_remapper.map(ys),
+                zs: z_remapper.map(zs),
+            },
+        )
+        .collect_vec();
+
+    let mut remapped_reactor = Array3::from_elem(
+        [
+            x_remapper.numbers.len() - 1,
+            y_remapper.numbers.len() - 1,
+            z_remapper.numbers.len() - 1,
+        ],
+        false,
+    );
+
+    for Instruction {
+        new_value,
+        xs,
+        ys,
+        zs,
+    } in &remapped_instructions
+    {
+        remapped_reactor
+            .slice_mut(s![xs.clone(), ys.clone(), zs.clone()])
+            .fill(*new_value);
+    }
+    println!(
+        "Task 2: {}",
+        remapped_reactor
+            .indexed_iter()
+            .map(|((i, j, k), v)| (*v as u128)
+                * (x_remapper.region_size(i) as u128)
+                * (y_remapper.region_size(j) as u128)
+                * (z_remapper.region_size(k) as u128))
+            .sum::<u128>()
+    );
 
     Ok(())
 }
